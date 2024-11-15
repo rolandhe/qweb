7b2a7ffffc3a5e1ef0fce29f9c575c469f660b98
xiufeng he <hexiufeng@xiufengdeMacBook-Pro.local>
2024-11-16 00:35:26 +0800

    change muliti mem

--- requests/engine.go
+++ requests/engine.go
@@ -5,13 +5,15 @@ import (
 	"github.com/rolandhe/qweb/profile"
 )
 
+const MaxMultipartMemory = 2 << 20
+
 func NewEngine() *gin.Engine {
 	if profile.IsProd() {
 		gin.SetMode(gin.ReleaseMode)
 	}
 	e := gin.New()
 	e.UseH2C = true
-	e.MaxMultipartMemory = 2 << 20
+	e.MaxMultipartMemory = MaxMultipartMemory
 	e.Use(recoverHandler(), corsHandler(), monitorHandler(), healthHandler())
 	_ = e.SetTrustedProxies(nil)
 	e.HandleMethodNotAllowed = true
